8fcc0bd4dcf8f899d4a255cb3527b552951d1453
Craig Weber <crgwbr@gmail.com>
2018-02-08 18:57:15 -0500

    Run tests against pypy3

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -1,6 +1,9 @@
 from oscar.defaults import *  # noqa
 from oscar import OSCAR_MAIN_TEMPLATE_DIR, get_core_apps
+from psycopg2cffi import compat
 import os
+
+compat.register()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
