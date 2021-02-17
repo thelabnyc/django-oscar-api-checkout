ed9f17c29eafde30627b84d013732755dec9c3c9
Craig Weber <crgwbr@gmail.com>
2021-02-17 09:50:36 -0500

    Refs #17236 Oscar 3.0 Compatibility

--- src/oscarapicheckout/tests/test_checkout.py
+++ src/oscarapicheckout/tests/test_checkout.py
@@ -2202,7 +2202,7 @@ class CheckoutAPITest(BaseTest):
             currency='USD',
             product=product,
             num_in_stock=10,
-            price_excl_tax=price)
+            price=price)
         factories.create_purchase_info(record)
         return product
 
