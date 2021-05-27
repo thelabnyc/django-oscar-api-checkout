c260313cb6458f55ce17562f9389d2a3595da790
Craig Weber <crgwbr@gmail.com>
2021-05-27 09:49:53 -0400

    Remove deprecated providing_args argument to signals

--- src/oscarapicheckout/signals.py
+++ src/oscarapicheckout/signals.py
@@ -1,6 +1,13 @@
 from django.dispatch import Signal
 
-pre_calculate_total = Signal(providing_args=["basket", "shipping_address", "shipping_charge"])
-order_placed = Signal(providing_args=["order", "user", "request"])
-order_payment_authorized = Signal(providing_args=["order", "request"])
-order_payment_declined = Signal(providing_args=["order", "request"])
+# providing_args=["basket", "shipping_address", "shipping_charge"]
+pre_calculate_total = Signal()
+
+# providing_args=["order", "user", "request"]
+order_placed = Signal()
+
+# providing_args=["order", "request"]
+order_payment_authorized = Signal()
+
+# providing_args=["order", "request"]
+order_payment_declined = Signal()
