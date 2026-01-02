53b6480fddd5447500a758763c866593bc53f58f
Craig Weber <crgwbr@gmail.com>
2026-01-02 11:20:43 -0500

    chore: update copyright year

--- sandbox/creditcards/methods.py
+++ sandbox/creditcards/methods.py
@@ -5,8 +5,6 @@ from django.core.signing import Signer
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 from oscar.apps.payment.models import Transaction
-from rest_framework.reverse import reverse
-
 from oscarapicheckout.methods import (
     PaymentMethod,
     PaymentMethodData,
@@ -18,6 +16,7 @@ from oscarapicheckout.states import (
     FormPostRequired,
     FormPostRequiredFormDataField,
 )
+from rest_framework.reverse import reverse
 
 if TYPE_CHECKING:
     from ..order.models import Order
